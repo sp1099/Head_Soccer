b176f28cb95fddfda1e9d7931d0cac994ff1f777
Alexander Za <a.zauritz@gmx.de>
2020-06-01 15:54:36 +0200

    test function

--- ball.py
+++ ball.py
@@ -243,8 +243,8 @@ class Ball(pygame.sprite.Sprite):
         
         import sys
 
-        player.temp = [2.1, -1.7, 2.1, -1.7, "speedx", sys.float_info.max]
-        ball.temp = [-3.5, 5.9, 3.5, -5.9, None, sys.float_info.max]
+        player.temp = [0, 7.4, 2.1, -1.7, 2.1, -1.7, "speedx", sys.float_info.max]
+        ball.temp = [7.4, 0, -3.5, 5.9, 3.5, -5.9, None, sys.float_info.max]
         message = "New speedx value is {speedx}"
 
         for i in range(6):
